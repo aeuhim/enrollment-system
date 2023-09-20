216391d48926a6a29fa73f52698133982e435c4b
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-09-21 03:14:06 +0800

    Update accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -11,9 +11,6 @@ class CustomUser(AbstractUser):
     name_suffix = models.CharField(_("name suffix"), max_length=8, blank=True)
     email = models.EmailField(_("email address"), unique=True)
     contact_number = models.CharField(_("contact number"), max_length=(16), unique=True)
-    permanent_address = models.CharField(_("permanent address"), max_length=128)
-    current_address = models.CharField(_("current address"), max_length=128)
-    emergency_number = models.CharField(_("emergency contact number"), max_length=16)
 
     def __str__(self):
-        return " ".join([self.first_name, self.middle_name, self.last_name])
+        return " ".join([self.first_name, self.middle_name, self.last_name, self.name_suffix])
